47429ed446a866c606e49ee7d795d093432452d9
taminhnhat <taminhnhat98@gmail.com>
2023-03-02 16:38:33 +0700

    i

---
+++ bot-client/server.js
@@ -0,0 +1,13 @@
+const { io } = require("socket.io-client")
+const socket = io('http://192.168.1.42:3000')
+
+socket.on("start", (data) => {
+    console.log(data)
+});
+socket.on('bot:status', () => {
+    socket.emit('bot:status', { status: '' })
+})
+socket.on('bot:pathLog', () => {
+    Socket.emit('bot:pathLog', { id: 'Y-004', des: 'G-2-34', dir: '90', path: [], pos: 'G-2-12', cor: '2200:12340:89' })
+})
+socket.emit('bot:init', { bot_id: 'Y-004' })
--- bot-server/server.js
+++ bot-server/server.js
@@ -2,40 +2,30 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-
-const botController = require('.')
+const controller = require('./controller');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const {
-    createTodo,
-    readTodo,
-    updateTodo,
-    deleteTodo,
-    listTodo,
-} = createTodoHandlers(components);
-
 io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    });
-    socket.on("picking:newPlan", createTodo);
-    socket.on("picking:check", readTodo);
+    console.log(socket.conn.remoteAddress, socket.conn.id)
+    io.emit('start', { message: `New bot ${socket.conn.id} connected` })
+    socket.emit('bot:status', {})
+    socket.on("picking:accepted", controller.handlePickingAccepted);
+    socket.on("picking:complete", controller.handlePickingComplete);
+    socket.on("picking:error", controller.handlePickingError);
 
-    socket.on("putting:newPlan", updateTodo);
-    socket.on("putting:check", deleteTodo);
+    socket.on("putting:accepted", controller.handlePuttingAccepted);
+    socket.on("putting:complete", controller.handlePuttingComplete);
+    socket.on("putting:error", controller.handlePuttingError);
 
-    socket.on("bot:complete", listTodo);
-    socket.on("bot:status", handleBotStatus);
+    socket.on("bot:init", controller.handleBotInit);
+    socket.on("bot:status", controller.handleBotStatus);
+    socket.on("bot:error", controller.handleBotError);
+    socket.on('bot:pathLog', controller.handleBotPathLog);
 });
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
-
-function handleBotStatus() {
-    //
-}
