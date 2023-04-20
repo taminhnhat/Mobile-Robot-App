807f41d2c6fade16e0c3a5022a90243bda01650b
taminhnhat <taminhnhat98@gmail.com>
2023-04-20 17:27:19 +0700

    i

---
+++ agv-driver/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  Serial.println("back pill call loop");
+  delay(3000);
+  // put your main code here, to run repeatedly:
+}
