cf778d4c503b3169e247f0287466a0adb405827e
taminhnhat <taminhnhat98@gmail.com>
2023-05-26 17:06:36 +0700

    i

--- agv-driver/src/main.cpp
+++ agv-driver/src/main.cpp
@@ -40,15 +40,17 @@ const float wheel_diameter = 0.09;  // m
 const float motor_max_speed = 333;  // rpm
 const float angular_velocity_factor = (wheel_distance * wheel_distance + wheel_separation * wheel_separation) / (2 * wheel_separation);
 const float wheel_speed_factor = 60 / (3.1416 * wheel_diameter);
+const float max_linear_velocity = motor_max_speed / wheel_speed_factor;
+const float max_angular_velocity = (2 * motor_max_speed) / (wheel_speed_factor * wheel_separation);
 const uint32_t pwm_max_val = pow(2, PWM_RESOLUTION) - 1;
 class MotorControl
 {
 private:
-  const float ppr = 2970.0;
+  const float ppr = 330.0;
+  const uint32_t vel_cal_cycle = 50;
   uint32_t a;
   uint32_t b;
   uint32_t p;
-  uint32_t dp;
   float v;
   uint32_t last_t;
   uint32_t last_p;
@@ -67,17 +69,19 @@ private:
 
   void tick()
   {
+    const uint32_t present_p = this->p;
     const uint32_t d_t = millis() - this->last_t;
-    if (d_t >= 100)
+    if (d_t >= this->vel_cal_cycle)
     {
-      const uint32_t d_p = this->p - this->last_p;
-      this->v = (d_p * 60) / (d_t * this->ppr);
-      last_t = millis();
-      last_p = this->p;
+      this->d_p = present_p - this->last_p;
+      this->v = (this->d_p * 60) / (d_t * this->ppr);
+      this->last_t += this->vel_cal_cycle;
+      this->last_p = present_p;
     }
   }
 
 public:
+  uint32_t d_p;
   MotorControl(uint32_t direction_pin, uint32_t pwm_pin)
   {
     this->a = 0;
@@ -131,18 +135,41 @@ public:
   }
   void drive(float velocity)
   {
-    const float wheel_speed = abs(velocity) * wheel_speed_factor;      // rpm
-    const float pwm_val = wheel_speed * pwm_max_val / motor_max_speed; // %
+    const float wheel_speed = abs(velocity) * wheel_speed_factor;         // rpm
+    const uint32_t pwm_val = wheel_speed * pwm_max_val / motor_max_speed; // %
     if (velocity > 0)
-    {
-      digitalWrite(this->DIR_PIN_ADDR, LOW);
-      analogWrite(this->PWM_PIN_ADDR, pwm_val);
-    }
-    else
     {
       digitalWrite(this->DIR_PIN_ADDR, HIGH);
       analogWrite(this->PWM_PIN_ADDR, pwm_max_val - pwm_val);
+      Serial1.print(pwm_max_val - pwm_val);
     }
+    else
+    {
+      digitalWrite(this->DIR_PIN_ADDR, LOW);
+      analogWrite(this->PWM_PIN_ADDR, pwm_val);
+      Serial1.print(pwm_val);
+    }
+    Serial1.print(":");
+  }
+  void rotate(float speed)
+  {
+    float wheel_speed = abs(speed); // rpm
+    if (wheel_speed > motor_max_speed)
+      wheel_speed = motor_max_speed;
+    uint32_t pwm_val = wheel_speed * pwm_max_val / motor_max_speed; // %
+    if (speed > 0)
+    {
+      digitalWrite(this->DIR_PIN_ADDR, HIGH);
+      analogWrite(this->PWM_PIN_ADDR, pwm_max_val - pwm_val);
+      Serial1.print(pwm_max_val - pwm_val);
+    }
+    else
+    {
+      digitalWrite(this->DIR_PIN_ADDR, LOW);
+      analogWrite(this->PWM_PIN_ADDR, pwm_val);
+      Serial1.print(pwm_val);
+    }
+    Serial1.print(":");
   }
   void fix()
   {
@@ -219,7 +246,7 @@ uint32_t tick_t = millis();
 String messageFromSerial1 = "";
 
 void msgProcess(String);
-void velocityProcess(float, float);
+void velocityProcess(double, double);
 
 // -------------------------------------------------TIMER--------------------------------------------------------
 HardwareTimer timer(TIM1);
@@ -277,14 +304,46 @@ void setup()
   timer.attachInterrupt(OnTimer1Interrupt);
   timer.refresh();
   timer.resume();
-  Serial1.print("timer fre: ");
+  Serial1.println("===============ROBOT START================");
+  Serial1.println("System information");
+  Serial1.print("timer freq: ");
   Serial1.println(timer.getTimerClkFreq());
+  Serial1.print("pwm resolution: ");
+  Serial1.println(pwm_max_val);
+  Serial1.print("max speed: ");
+  Serial1.print(max_linear_velocity);
+  Serial1.print(" m/s\tturn: ");
+  Serial1.print(max_angular_velocity);
+  Serial1.println(" rad/s");
+
+  motor1.stop();
+  motor2.stop();
+  motor3.stop();
+  motor4.stop();
 }
 
 void loop()
 {
-  if (millis() - tick_t > 10000)
+  const uint32_t cycle = 1000;
+  const uint32_t t = millis();
+  if (t - tick_t >= cycle)
   {
+    Serial1.print(motor1.d_p);
+    Serial1.print(':');
+    Serial1.print(motor2.d_p);
+    Serial1.print(':');
+    Serial1.print(motor3.d_p);
+    Serial1.print(':');
+    Serial1.print(motor4.d_p);
+    Serial1.print('\t');
+    Serial1.print(motor1.pulses());
+    Serial1.print(':');
+    Serial1.print(motor2.pulses());
+    Serial1.print(':');
+    Serial1.print(motor3.pulses());
+    Serial1.print(':');
+    Serial1.print(motor4.pulses());
+    Serial1.print('\t');
     Serial1.print(motor1.velocity());
     Serial1.print(':');
     Serial1.print(motor2.velocity());
@@ -293,7 +352,7 @@ void loop()
     Serial1.print(':');
     Serial1.print(motor4.velocity());
     Serial1.println('.');
-    tick_t = millis();
+    tick_t += cycle;
   }
 }
 
@@ -332,13 +391,14 @@ void msgProcess(String lightCmd)
   const String topic_name = String(topic);
   if (topic_name.compareTo("control") == 0)
   {
-    Serial1.println("control velocity");
     const double linear = doc["linear"];
     const double angular = doc["angular"];
-    Serial1.print("linear: ");
-    Serial1.print(linear);
-    Serial1.print("\tangular: ");
-    Serial1.println(angular);
+    // Serial1.println("control velocity");
+    // Serial1.print("linear: ");
+    // Serial1.print(linear);
+    // Serial1.print("\tangular: ");
+    // Serial1.println(angular);
+    velocityProcess(linear, angular);
   }
   else if (topic_name.compareTo("state") == 0)
   {
@@ -350,12 +410,31 @@ void msgProcess(String lightCmd)
   }
 }
 
-void velocityProcess(float linear, float angular)
+void velocityProcess(double linear, double angular)
 {
-  const float motor_1_velocity = linear - angular * angular_velocity_factor;
-  const float motor_2_velocity = linear - angular * angular_velocity_factor;
-  const float motor_3_velocity = linear + angular * angular_velocity_factor;
-  const float motor_4_velocity = linear + angular * angular_velocity_factor;
+  const double angular_tmp = angular * angular_velocity_factor;
+  double linear_tmp = linear;
+  const double motor_1_velocity = linear_tmp - angular_tmp;
+  const double motor_2_velocity = linear_tmp - angular_tmp;
+  const double motor_3_velocity = linear_tmp + angular_tmp;
+  const double motor_4_velocity = linear_tmp + angular_tmp;
+  // Serial1.print("WHEEL VELOCITY: ");
+  // Serial1.print(motor_1_velocity);
+  // Serial1.print("\t");
+  // Serial1.print(motor_2_velocity);
+  // Serial1.print("\t");
+  // Serial1.print(motor_3_velocity);
+  // Serial1.print("\t");
+  // Serial1.print(motor_4_velocity);
+  // Serial1.print("\tWHEEL SPEED: ");
+  // Serial1.print(motor_1_velocity * wheel_speed_factor);
+  // Serial1.print("\t");
+  // Serial1.print(motor_2_velocity * wheel_speed_factor);
+  // Serial1.print("\t");
+  // Serial1.print(motor_3_velocity * wheel_speed_factor);
+  // Serial1.print("\t");
+  // Serial1.print(motor_4_velocity * wheel_speed_factor);
+  // Serial1.println("");
   motor1.drive(motor_1_velocity);
   motor2.drive(motor_2_velocity);
   motor3.drive(motor_3_velocity);
