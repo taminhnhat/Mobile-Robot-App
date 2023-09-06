0e99411e54865c1c74352b487226dca9b1e484ce
taminhnhat <taminhnhat98@gmail.com>
2023-09-06 16:56:22 +0700

    i

--- ros-websocket/index.js
+++ ros-websocket/index.js
@@ -13,16 +13,20 @@ rclnodejs.init()
         const node = rclnodejs.createNode('subscription_example_node');
         console.log('created node')
 
-        node.createSubscription('geometry_msgs/msg/Twist', 'cmd_vel_joy', (msg) => {
-            console.log(`Received message: ${typeof msg}`, msg);
-            socket.emit('ros:topic', {
-                topic: 'cmd_vel_joy',
-                message: msg
-            })
-        });
-        const publisher = node.createPublisher('std_msgs/msg/String', 'ws_vel');
+        // node.createSubscription('geometry_msgs/msg/Twist', 'cmd_vel_joy', (msg) => {
+        //     console.log(`Received message: ${typeof msg}`, msg);
+        //     socket.emit('ros:topic', {
+        //         topic: 'cmd_vel_joy',
+        //         message: msg
+        //     })
+        // });
+        const publisher = node.createPublisher('geometry_msgs/msg/Twist', 'ws_vel');
         socket.on('ros:topic', d => {
-            publisher.publish(d);
+            console.log(d)
+            publisher.publish({
+                linear: { x: d.data.linear[0], y: d.data.linear[1], z: d.data.linear[2] },
+                angular: { x: d.data.angular[0], y: d.data.angular[1], z: d.data.angular[2] },
+            });
             console.log('ros:topic', d)
         })
 
