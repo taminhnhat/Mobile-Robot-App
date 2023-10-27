892014dfffd121f2f882ae80fbcce38369a8852f
taminhnhat <taminhnhat98@gmail.com>
2023-10-27 17:26:20 +0700

    i

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 const { io } = require("socket.io-client")
+const { exec, execSync } = require('child_process');
+const { Worker } = require('worker_threads');
 require('dotenv').config({ path: './.env' })
 const url = process.env.SERVER_URL || 'http://localhost:3003'
 console.log(`connecting to ${url}`)
@@ -9,52 +11,75 @@ socket.on('disconnect', () => console.log('disconnected', socket.id))
 socket.on('error', err => console.log('error', err))
 socket.on('tick', t => console.log('tick', t))
 
-const { exec, execSync } = require('child_process');
-let rosMonitorData = {
+let robotMonitorData = {
     nodes: [],
-    topics: []
+    topics: [],
+    services: [],
+    nodesUpdated: false,
+    topicsUpdated: false,
 }
-setInterval(() => exec('ros2 node list', (err, stdout, stderr) => {
-    if (err) {
-        console.error(err)
-    } else {
-        // the *entire* stdout and stderr (buffered)
-        if (stdout) {
-            // console.log(`stdout: ${stdout}`)
-            let d = stdout.trim()
-            let nodesName = d.split('\n')
-            rosMonitorData.nodes = nodesName
+
+getRosInfo()
+function getRosInfo() {
+    let wk = new Worker(__dirname + '/threads/rosNode.js')
+    let wk2 = new Worker(__dirname + '/threads/rosTopic.js')
+
+    wk2.on('message', (msg) => {
+        robotMonitorData.topics = msg
+        robotMonitorData.topicsUpdated = true
+        if (rosInfoCheck()) {
+            wk.postMessage('run')
+            wk2.postMessage('run')
         }
-        if (stderr) console.log(`stderr: ${stderr}`)
+    });
+
+    wk.on('error', err => console.log(err));
+
+    wk.on('exit', (code) => {
+        if (code !== 0) console.error(new Error(`Worker stopped Code ${code}`))
+    });
+
+    wk.on('message', (msg) => {
+        robotMonitorData.nodes = msg
+        robotMonitorData.nodesUpdated = true
+        if (rosInfoCheck()) {
+            wk.postMessage('run')
+            wk2.postMessage('run')
+        }
+    });
+
+    wk2.on('error', err => console.log(err));
+
+    wk2.on('exit', (code) => {
+        if (code !== 0) console.error(new Error(`Worker stopped Code ${code}`))
+    });
+    // setTimeout(getRosState, 2000)
+}
+
+getServiceInfo()
+function getServiceInfo() {
+    let wk = new Worker(__dirname + '/threads/userService.js')
+
+    wk.on('message', (msg) => {
+        robotMonitorData.services = msg
+    });
+
+    wk.on('error', err => console.log(err));
+
+    wk.on('exit', (code) => {
+        if (code !== 0) console.error(new Error(`Worker stopped Code ${code}`))
+    });
+}
+
+function rosInfoCheck() {
+    if (robotMonitorData.nodesUpdated === true && robotMonitorData.topicsUpdated === true) {
+        robotMonitorData.nodesUpdated = false
+        robotMonitorData.topicsUpdated = false
+        socket.emit('ros:monitor', robotMonitorData)
+        return true
     }
-}), 1000)
-
-function getRosState() {
-    exec('ros2 topic list', (err, stdout, stderr) => {
-        if (err) {
-            console.error(err)
-        } else {
-            // the *entire* stdout and stderr (buffered)
-            if (stdout) {
-                // console.log(`stdout: ${stdout}`)
-                let d = stdout.trim()
-                rosMonitorData.topics = []
-                let topicsName = d.split('\n')
-                topicsName.forEach(async (tp) => {
-                    let out = execSync(`ros2 topic info ${tp}`)
-                    // console.log(String(out))
-                    if (err) console.log(err)
-                    rosMonitorData.topics.push({ name: tp, type: String(out).trim().split(' ')[1] })
-                })
-            }
-            if (stderr) console.log(`stderr: ${stderr}`)
-            socket.emit('ros:monitor', rosMonitorData)
-        }
-    })
-    setTimeout(getRosState, 1000)
+    else return false
 }
-
-getRosState()
 
 const fs = require('fs');
 const fileHandle = fs.openSync('/tmp/ros2_control_out', 'r+');
@@ -75,6 +100,7 @@ fifoRs.on('close', function (err) {
 fifoRs.on('data', d => {
     const dStr = String(d).substring(d.lastIndexOf('{'), d.lastIndexOf('}') + 1)
     const dJson = JSON.parse(dStr)
+    // console.log('pipe out >>>')
     // console.log(dJson.vel)
     let dPro = JSON.parse(JSON.stringify(dJson))
     delete dPro.topic
