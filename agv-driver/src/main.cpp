fa654e7ea87cf9170d0d816edef29f826cc8515f
taminhnhat <taminhnhat98@gmail.com>
2024-01-05 14:46:56 +0700

    i

--- agv-driver/src/main.cpp
+++ agv-driver/src/main.cpp
@@ -594,7 +594,7 @@ void lcdRender()
   String Wat = String() + 19.1 + "W";
   display.drawString(14, 0, Wat.c_str(), OLED::NORMAL_SIZE, OLED::WHITE);
   String Ip = String() + "192.168.1.44";
-  uint8_t batteryScale = 42.0 * battery.getAverageVoltage() / 16.8;
+  uint8_t batteryScale = 42.0 * (battery.getAverageVoltage() - 12.8) / 4;
   display.draw_rectangle(80, 15, 125, 31, OLED::HOLLOW, OLED::WHITE);
   display.draw_rectangle(125, 18, 127, 27, OLED::SOLID, OLED::WHITE);
   display.draw_rectangle(82, 17, 81 + batteryScale, 29, OLED::SOLID, OLED::WHITE);
